2f271fd153d0519f076dbf50bd1e17d50ed86c7a
Serhii Sharyi <serhiisharyi77@gmail.com>
2023-03-19 02:19:35 +0200

    init

---
+++ src/AddUserView/AddUserView.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import style from "./style.module.css";
+
+const AddUserView = ({ addUser }) => {
+  const [form, setForm] = useState({ name: "", email: "" });
+
+  const handleUpdateForm = (event) => {
+    event.preventDefault();
+    const { value, id } = event.target;
+    setForm((form) => ({ ...form, [id]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addUser(form);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={style.addUserForm}>
+      <div className={style.addUserFieldContainer}>
+        <label htmlFor="email">email</label>
+        <input
+          onChange={handleUpdateForm}
+          value={form.email}
+          type="email"
+          id="email"
+        />
+      </div>
+      <div className={style.addUserFieldContainer}>
+        <label htmlFor="name">username</label>
+        <input id="name" value={form.name} onChange={handleUpdateForm} />
+      </div>
+      <button className={style.submitNewUserButton} type="submit">
+        Add User
+      </button>
+    </form>
+  );
+};
+
+export default AddUserView;
